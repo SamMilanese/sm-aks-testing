49ade42bd6580765ed0b8f8e032e77442ef10a43
SamMilanese <44896496+SamMilanese@users.noreply.github.com>
2023-10-31 17:02:20 +0000

    Update pod_tfc_agent.tf

--- pod_tfc_agent.tf
+++ pod_tfc_agent.tf
@@ -13,6 +13,18 @@ resource "kubernetes_pod_v1" "pod_tfc_agent" {
         name  = "environment"
         value = "dev"
       }
+      env {
+            name  = "TFC_AGENT_TOKEN"
+            value = var.tfc_agent_token
+          }
+      env {
+            name  = "TFC_AGENT_NAME"
+            value = "SM_Test_Agent_1"
+          }
+      env {
+            name  = "TFC_AGENT_SINGLE"
+            value = "true"
+          }
 
       port {
         container_port = 8080
