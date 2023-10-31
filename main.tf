e22059fd9866420cb9e93e7ebcb143904e847b66
SamMilanese <44896496+SamMilanese@users.noreply.github.com>
2023-10-31 14:39:53 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -60,7 +60,6 @@ provider "kubernetes" {
   client_certificate     = "${base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate)}"
   client_key             = "${base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_key)}"
   cluster_ca_certificate = "${base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate)}"
-  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate)
 
   # using kubelogin to get an AAD token for the cluster.
   exec {
