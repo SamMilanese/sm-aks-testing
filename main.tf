ba2c847782258f94a59898174812464bdd9bdd37
SamMilanese <44896496+SamMilanese@users.noreply.github.com>
2023-10-31 14:45:32 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -70,13 +70,13 @@ provider "kubernetes" {
       "--environment",
       "AzurePublicCloud",
       "--server-id",
-      data.azuread_service_principal.aks_aad_server.application_id, # Note: The AAD server app ID of AKS Managed AAD is always 6dae42f8-4368-4678-94ff-3960e28e3630 in any environments.
+      6dae42f8-4368-4678-94ff-3960e28e3630, # Note: The AAD server app ID of AKS Managed AAD is always 6dae42f8-4368-4678-94ff-3960e28e3630 in any environments.
       "--client-id",
-      azuread_application.app.application_id, # SPN App Id created via terraform
+      var.appId, # SPN App Id created via terraform
       "--client-secret",
-      azuread_service_principal_password.spn_password.value,
+      var.password,
       "--tenant-id",
-      data.azurerm_subscription.current.tenant_id, # AAD Tenant Id
+      var.tenantID, # AAD Tenant Id
       "--login",
       "spn"
     ]
