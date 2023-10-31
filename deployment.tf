24fe0aabff9c9238e11b7ea31c625622935593f7
SamMilanese <44896496+SamMilanese@users.noreply.github.com>
2023-10-31 13:35:00 +0000

    Create deployment.tf

---
+++ deployment.tf
@@ -0,0 +1,61 @@
+resource "kubernetes_deployment_v1" "deploy" {
+  metadata {
+    name      = "deploy-tfc-agent"
+    namespace = kubernetes_namespace_v1.ns.metadata.0.name
+
+    labels = {
+      tier = "frontend"
+    }
+  }
+
+  spec {
+    replicas = 3
+
+    selector {
+      match_labels = {
+        tier = "frontend"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          tier = "frontend"
+        }
+      }
+
+      spec {
+        container {
+          image = "hashicorp/tfc-agent:latest"
+          name  = "tfc-agent"
+
+          resources {
+            limits = {
+              cpu    = "1"
+              memory = "256Mi"
+            }
+            requests = {
+              cpu    = "500m"
+              memory = "30Mi"
+            }
+          }
+
+          liveness_probe {
+            http_get {
+              path = "/"
+              port = 8080
+
+              http_header {
+                name  = "X-Custom-Header"
+                value = "GreatBlogArticle"
+              }
+            }
+
+            initial_delay_seconds = 2
+            period_seconds        = 2
+          }
+        }
+      }
+    }
+  }
+}
