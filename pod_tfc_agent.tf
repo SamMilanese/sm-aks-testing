8ac5cd6798f7be58db40fc35e4e58c774795004b
SamMilanese <44896496+SamMilanese@users.noreply.github.com>
2023-10-31 17:40:30 +0000

    Update pod_tfc_agent.tf

--- pod_tfc_agent.tf
+++ pod_tfc_agent.tf
@@ -30,17 +30,6 @@ resource "kubernetes_pod_v1" "pod_tfc_agent" {
         container_port = 8080
       }
 
-      liveness_probe {
-        http_get {
-          path = "/"
-          port = 8080
-
-          http_header {
-            name  = "X-Custom-Header"
-            value = "GreatBlogArticle"
-          }
-        }
-
         initial_delay_seconds = 2
         period_seconds        = 2
       }
