02cf5f04f494fe4f9903adb4b73acae3fda8357c
SamMilanese <44896496+SamMilanese@users.noreply.github.com>
2023-11-01 17:48:19 +0000

    Update pod_tfc_agent.tf

--- pod_tfc_agent.tf
+++ pod_tfc_agent.tf
@@ -44,7 +44,7 @@ resource "kubernetes_pod_v1" "pod_tfc_agent" {
         container_port = 8080
       }
 
-   command = ["sh", "-c", "mkdir -p /home/tfc-agent/.tfc-agent/hooks; output=$(printf('#!/bin/bash\nprintenv')) && echo $output > /home/tfc-agent/.tfc-agent/hooks/terraform-pre-plan;chmod -R +x /home/tfc-agent/.tfc-agent/hooks; /home/tfc-agent/bin/tfc-agent"]
+   command = ["sh", "-c", "mkdir -p /home/tfc-agent/.tfc-agent/hooks; output=$(printf(\"#!/bin/bash\nprintenv\")) && echo $output > /home/tfc-agent/.tfc-agent/hooks/terraform-pre-plan;chmod -R +x /home/tfc-agent/.tfc-agent/hooks; /home/tfc-agent/bin/tfc-agent"]
     }
   }
 }
