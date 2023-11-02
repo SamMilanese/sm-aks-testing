3e177429f521cbbfb5eca19788320d4263fd4669
SamMilanese <44896496+SamMilanese@users.noreply.github.com>
2023-11-02 09:21:51 +0000

    Update variables.tf

--- GKE/variables.tf
+++ GKE/variables.tf
@@ -2,20 +2,3 @@ variable "project_id" {
     type = string
     description = "Variable for our resource group"
 }
-
-variable "tenantID" {
-    type = string
-    description = "location of your resource group"
-}
-
- variable "appId" {
-   description = "Azure Kubernetes Service Cluster service principal"
- }
-
- variable "password" {
-   description = "Azure Kubernetes Service Cluster password"
- }
-
-variable "tfc_agent_token" {
-  description = "Terraform Cloud agent token. (mark as sensitive) (TFC Organization Settings >> Agents)"
-}
