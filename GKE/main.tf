a576a264aa8af310fa44ed7b817ef39b98029d55
SamMilanese <44896496+SamMilanese@users.noreply.github.com>
2023-11-02 09:01:09 +0000

    Update main.tf

--- GKE/main.tf
+++ GKE/main.tf
@@ -2,3 +2,16 @@ provider "google" {
   project = var.project_id
   region  = "us-central1"
 }
+
+terraform {
+  backend "gcs" {
+    bucket = "sm_bucket"
+    prefix = "terraform/state"
+  }
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+}
